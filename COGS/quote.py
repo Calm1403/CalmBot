da4626e4e9e14ea5e4fba578d8a7ef555b635465
Calm1403 <harris.berry@outlook.com>
2023-09-09 12:22:49 +0100

    Added quote command.

---
+++ COGS/quote.py
@@ -0,0 +1,24 @@
+from discord.ext import commands
+from colorama import Fore as F
+import requests
+import discord
+
+
+class quote(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"~~~ The quote.py cog has been {F.GREEN}loaded{F.RESET}! ~~~")
+
+    @commands.command(aliases=["quote"])
+    async def send_quote(self, ctx):
+        request = requests.get("https://quotenjoke.vercel.app/quote")
+        quote_to_be_sent = request.json()
+
+        await ctx.send(f"{ctx.author.mention}: Here you are!\n\"{quote_to_be_sent['content']}\" - {quote_to_be_sent['author']}")
+
+
+async def setup(client):
+    await client.add_cog(quote(client))
