88ef93b06b25b9d41b2715c002275ac2f40176d0
Calm1403 <harris.berry@outlook.com>
2023-09-24 09:19:17 +0100

    Added asyncronous functionality? (Maybe; untested)

--- COGS/quote.py
+++ COGS/quote.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 from colorama import Fore as F
-import requests
+import aiohttp
 import discord
 
 
@@ -15,14 +15,12 @@ class quote(commands.Cog):
     @commands.command(aliases=["quote"])
     async def send_quote(self, ctx):
         # This command will send a quote.
-        request = requests.get("https://quotenjoke.onrender.com/quote")
-        quote_to_be_sent = request.json()
 
-        if request.status_code == 500:
-            await ctx.send(f"{ctx.author.mention}: Sorry.. there was a problem with the request.")
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://quotenjoke.onrender.com/quote") as request:
+                quote_to_be_sent = await request.json()
 
-        else:
-            await ctx.send(f"{ctx.author.mention}: \"{quote_to_be_sent['content']}\" - {quote_to_be_sent['author']}")
+                await ctx.send(f"{ctx.author.mention}: \"{quote_to_be_sent['content']}\" - {quote_to_be_sent['author']}")
 
 
 async def setup(client):
