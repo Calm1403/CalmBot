d642eefbadc1ff0b6c2f22bc93053f820699533c
Calm1403 <harris.berry@outlook.com>
2023-10-06 17:09:19 +0100

    Edited fox file.

--- COGS/fox.py
+++ COGS/fox.py
@@ -18,6 +18,9 @@ class fox(commands.Cog):
 
         async with aiohttp.ClientSession() as session:
             async with session.get("https://randomfox.ca/floof/") as request:
+                if request.status != 200:
+                    return await ctx.send(f"{ctx.author.mention}: Sorry, there was a problem with the request.. {request.status}")
+
                 fox_to_be_sent = await request.json()
 
                 await ctx.send(f"{ctx.author.mention}: Here you are! {fox_to_be_sent['image']}")
