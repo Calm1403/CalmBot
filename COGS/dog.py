c913d1b84e38e5a60007aa827e1936ae255cb69c
Calm1403 <harris.berry@outlook.com>
2023-10-06 17:10:12 +0100

    Edited dog command.

--- COGS/dog.py
+++ COGS/dog.py
@@ -17,6 +17,8 @@ class dog(commands.Cog):
         # This command will send a picture of a dog.
         async with aiohttp.ClientSession() as session:
             async with session.get("https://dog.ceo/api/breeds/image/random") as request:
+                if request.status != 200:
+                    return await ctx.send(f"{ctx.author.mention}: Sorry, there was a problem with the request.. {request.status}")
                 dog_to_be_sent = await request.json()
 
         await ctx.send(f"{ctx.author.mention}: Here you are! {dog_to_be_sent['message']}")
