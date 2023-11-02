680aa5f5165e9fae07b9ea0a62b1be046f6bf831
Calm1403 <harris.berry@outlook.com>
2023-11-02 16:55:58 +0000

    Fixed silly mistake.

--- COGS/hello.py
+++ COGS/hello.py
@@ -19,7 +19,7 @@ class hello(commands.Cog):
         with open("Python/BOT/COGS/greetings.txt") as random_greetings_file:
             random_greeting = random.choice(random_greetings_file.readlines())
 
-            if random_greeting(len(random_greeting) - 1) == "\n":
+            if random_greeting[len(random_greeting) - 1] == "\n":
                 await ctx.send(f"{ctx.author.mention}: {random_greeting[:-1]}")
                 return
 
