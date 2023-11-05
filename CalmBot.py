35ced3ce136ee3de093f1764d291ed8e3d970962
Calm1403 <harris.berry@outlook.com>
2023-11-05 10:10:30 +0000

    Edited main file's comments.

--- CalmBot.py
+++ CalmBot.py
@@ -9,7 +9,7 @@ client = commands.Bot(command_prefix="$",
 
 
 @client.event
-# This decorator will state when the is active.
+# This decorator will state when the bot is active.
 async def on_ready():
     os.system("clear")
 
