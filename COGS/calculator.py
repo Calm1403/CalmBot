ad59e50573454d215bf6d166b58e0380ba284f37
Calm1403 <harris.berry@outlook.com>
2024-01-04 11:07:09 +0000

    Edited calculator file.

--- COGS/calculator.py
+++ COGS/calculator.py
@@ -76,40 +76,39 @@ class calculator(commands.Cog):
 
         if operator.content == "*":
             output = number_1 * number_2
-            await ctx.send(
+            return await ctx.send(
                 f"{ctx.author.mention}: {number_1} * {number_2} = {output}")
 
-        elif operator.content == "+":
+        if operator.content == "+":
             output = number_1 + number_2
-            await ctx.send(
+            return await ctx.send(
                 f"{ctx.author.mention}: {number_1} + {number_2} = {output}")
 
-        elif operator.content == "-":
+        if operator.content == "-":
             output = number_1 - number_2
-            await ctx.send(
+            return await ctx.send(
                 f"{ctx.author.mention}: {number_1} - {number_2} = {output}")
 
-        elif operator.content == "%":
+        if operator.content == "%":
             output = number_1 % number_2
-            await ctx.send(
+            return await ctx.send(
                 f"{ctx.author.mention}: {number_1} % {number_2} = {output}")
 
-        elif operator.content == "^":
+        if operator.content == "^":
             output = number_1 ^ number_2
-            await ctx.send(
+            return await ctx.send(
                 f"{ctx.author.mention}: {number_1} ^ {number_2} = {output}")
 
-        else:
-            try:
-                output = number_1 / number_2
+        try:
+            output = number_1 / number_2
 
-            except:
-                return await ctx.send(
-                    f"{ctx.author.mention}: I can't really divide {number_1} by {number_2} can I now?"
-                )
+        except:
+            return await ctx.send(
+                f"{ctx.author.mention}: I can't really divide {number_1} by {number_2} can I now?"
+            )
 
-            await ctx.send(
-                f"{ctx.author.mention}: {number_1} / {number_2} = {output}")
+        await ctx.send(
+            f"{ctx.author.mention}: {number_1} / {number_2} = {output}")
 
 
 async def setup(client):
