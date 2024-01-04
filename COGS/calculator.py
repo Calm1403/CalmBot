2f6d0d4ec8e7f5b1e9ce96f0f8b7b1d50451b7f8
Calm1403 <harris.berry@outlook.com>
2024-01-04 11:01:03 +0000

    Removed comments, imports, and fixed formatting.

--- COGS/calculator.py
+++ COGS/calculator.py
@@ -1,10 +1,10 @@
 from discord.ext import commands
 from colorama import Fore as F
 import asyncio
-import discord
 
 
 class calculator(commands.Cog):
+
     def __init__(self, client):
         self.client = client
 
@@ -16,48 +16,63 @@ class calculator(commands.Cog):
 
     @commands.command(aliases=["calc"])
     async def calculator(self, ctx):
-        # This command will act like that of a simple calculator.
+
         def check(m):
             return m.author == ctx.author
 
         await ctx.send(f"{ctx.author.mention}: Enter your first number!")
 
         try:
-            number_1 = await self.client.wait_for("message", check=check, timeout=10.0)
+            number_1 = await self.client.wait_for("message",
+                                                  check=check,
+                                                  timeout=10.0)
 
         except asyncio.TimeoutError:
-            return await ctx.send(f"{ctx.author.mention}: Fine.. don't enter your first number. :unamused:")
+            return await ctx.send(
+                f"{ctx.author.mention}: Fine.. don't enter your first number. :unamused:"
+            )
 
         try:
             number_1 = int(number_1.content)
 
         except:
-            return await ctx.send(f"{ctx.author.mention}: That's not a number yo.. :skull:")
+            return await ctx.send(
+                f"{ctx.author.mention}: That's not a number yo.. :skull:")
 
         await ctx.send(f"{ctx.author.mention}: Enter your operator!")
 
         try:
-            operator = await self.client.wait_for("message", check=check, timeout=10.0)
+            operator = await self.client.wait_for("message",
+                                                  check=check,
+                                                  timeout=10.0)
 
         except asyncio.TimeoutError:
-            return await ctx.send(f"{ctx.author.mention}: Fine.. don't enter your operator. :unamused:")
+            return await ctx.send(
+                f"{ctx.author.mention}: Fine.. don't enter your operator. :unamused:"
+            )
 
         if operator.content not in ("*", "/", "+", "-", "%", "^"):
-            return await ctx.send(f"{ctx.author.mention}: That's not an operator yo.. :skull:")
+            return await ctx.send(
+                f"{ctx.author.mention}: That's not an operator yo.. :skull:")
 
         await ctx.send(f"{ctx.author.mention}: Enter your second number!")
 
         try:
-            number_2 = await self.client.wait_for("message", check=check, timeout=10.0)
+            number_2 = await self.client.wait_for("message",
+                                                  check=check,
+                                                  timeout=10.0)
 
         except asyncio.TimeoutError:
-            return await ctx.send(f"{ctx.author.mention}: Fine.. don't enter your second number. :unamused:")
+            return await ctx.send(
+                f"{ctx.author.mention}: Fine.. don't enter your second number. :unamused:"
+            )
 
         try:
             number_2 = int(number_2.content)
 
         except:
-            return await ctx.send(f"{ctx.author.mention}: That's not a number yo.. :skull:")
+            return await ctx.send(
+                f"{ctx.author.mention}: That's not a number yo.. :skull:")
 
         if operator.content == "*":
             output = number_1 * number_2
@@ -89,7 +104,9 @@ class calculator(commands.Cog):
                 output = number_1 / number_2
 
             except:
-                return await ctx.send(f"{ctx.author.mention}: I can't really divide {number_1} by {number_2} can I now?")
+                return await ctx.send(
+                    f"{ctx.author.mention}: I can't really divide {number_1} by {number_2} can I now?"
+                )
 
             await ctx.send(
                 f"{ctx.author.mention}: {number_1} / {number_2} = {output}")
