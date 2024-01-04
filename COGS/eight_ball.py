1c5a7e8dc24474e312dbdccf5616d0d3eedbd259
Calm1403 <harris.berry@outlook.com>
2024-01-04 10:57:54 +0000

    Removed import, removed comment, formatted code.

--- COGS/eight_ball.py
+++ COGS/eight_ball.py
@@ -1,11 +1,11 @@
 from discord.ext import commands
 from colorama import Fore as F
 import asyncio
-import discord
 import random
 
 
 class eight_ball(commands.Cog):
+
     def __init__(self, client):
         self.client = client
 
@@ -17,25 +17,33 @@ class eight_ball(commands.Cog):
 
     @commands.command(aliases=["8ball"])
     async def eight_ball(self, ctx):
-        # This command will intake a question, then reply to that question with a random response.
+
         def check(m):
             return m.author == ctx.author
 
-        await ctx.send(f"{ctx.author.mention}: Hey, ask me a yes or no question! :smile:")
+        await ctx.send(
+            f"{ctx.author.mention}: Hey, ask me a yes or no question! :smile:")
 
         try:
-            question = await self.client.wait_for("message", check=check, timeout=10.0)
+            question = await self.client.wait_for("message",
+                                                  check=check,
+                                                  timeout=10.0)
 
         except asyncio.TimeoutError:
-            return await ctx.send(f"{ctx.author.mention}: Fine.. don't ask. :unamused:")
+            return await ctx.send(
+                f"{ctx.author.mention}: Fine.. don't ask. :unamused:")
 
         with open("Python/BOT/COGS/responses.txt") as random_responses_file:
             random_response = random.choice(random_responses_file.readlines())
 
         if random_response[len(random_response) - 1] == "\n":
-            return await ctx.send(f"{ctx.author.mention}: \"{question.content}\" - {random_response[:-1]}")
+            return await ctx.send(
+                f"{ctx.author.mention}: \"{question.content}\" - {random_response[:-1]}"
+            )
 
-        await ctx.send(f"{ctx.author.mention}: \"{question.content}\" - {random_response}")
+        await ctx.send(
+            f"{ctx.author.mention}: \"{question.content}\" - {random_response}"
+        )
 
 
 async def setup(client):
